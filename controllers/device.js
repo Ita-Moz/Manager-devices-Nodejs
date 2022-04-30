31536e60d3b05f36dc27973b882421745b2516ff
manh <luongvanmanh1929@gmail.com>
2022-04-30 14:56:55 +0700

    thieu Search

--- controllers/device.js
+++ controllers/device.js
@@ -1,11 +1,143 @@
 const devices = require('../models/device')
+var multer = require('multer');
 
+let fs = require('fs')
+//multer
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/image')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname)
+    }
+});
+var upload = multer({
+    storage: storage,
+    limits: {
+        fieldNameSize: 300,
+        fileSize: 10 * 1024 * 1024, // 10 Mb
+    },
+    fileFilter: function (req, file, cb) {
+        console.log(file);
+        cb(null, true)
+    }
+}).single("txtImage");
 
+//Hiển thị tất cả các thiết bị
+const showInfo = async (req, res) => {
+    try {
+        const allDevice = await devices.find();
+        return res.status(200).render('device', { devices: allDevice });
+    }
+    catch (err) {
+        return err;
+    }
+}
+// Hàm add device
+const addDevice = async (req, res) => {
+    await upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            console.log("Error when uploading.");
+        } else if (err) {
+            console.log("An unknown error " + err);
+        } else {
+            var name = req.body.txtNameDevice
+            devices.create({
+                deviceName: name.charAt(0).toUpperCase() + name.slice(1),
+                image: req.file.filename,
+                category: req.body.txtCategory,
+                quantity: req.body.txtQuantity,
+                price: req.body.txtPrice,
+                supplierName: req.body.txtSupplier,
+            })
+                .then(() => {
+                    res.redirect("http://localhost:5000/DeviceManager")
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        }
 
-const test = async (req, res) => {
-    res.render('device')
+    });
+}
+
+// Hàm xử lí xoá sản phẩm và xoá file ảnh của sản phẩm bên server
+const deleteDevice = async (req, res) => {
+    try {
+        await devices.deleteMany({ _id: req.params.id })
+        // GỠ FILE TRÊN SERVER
+        let path = `./public/image/${req.params.image}`
+        fs.unlink(path, function (err) {
+
+            if (err && err.code == 'ENOENT') {
+                // Lỗi tìm không thấy tệp, tệp không tồn tại.
+                console.info("File doesn't exist, won't remove it.");
+            } else if (err) {
+                // Đã xảy ra lỗi khi xóa tệp
+                console.error("Error occurred while trying to remove file");
+            } else {
+                console.info(`Đã xoá 1 ảnh trong server`);
+            }
+        });
+
+        return await res.status(200).send();
+    }
+    catch (err) {
+        return res.json(err);
+    }
+}
+// Hàm truyền giá trị vào modal
+const valUpdate = async (req, res) => {
+    try {
+        let deviceValUpdate = await devices.findOne({ _id: req.params.id });
+        return res.status(200).send(deviceValUpdate)
+    }
+    catch (err) {
+        return err;
+    }
+}
+// Hàm update device
+const updateDevice = async (req, res) => {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            console.log("Error when uploading.");
+        } else if (err) {
+            console.log("An unknown error " + err);
+        } else {
+            var name = req.body.name
+            devices.findOneAndUpdate({ _id: req.params.id }, {
+
+                $set: {
+                    deviceName: name.charAt(0).toUpperCase() + name.slice(1),
+                    category: req.body.category,
+                    price: req.body.price,
+                    quantity: req.body.quantity,
+                    supplierName: req.body.supplier,
+                }
+            }, (err, docs) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.status(200).send();
+                    console.log("Updated User : ", docs);
+                }
+            });
+        }
+
+    });
+}
+
+// Hàm search thiết bị theo Name
+
+const searchDevice = async (req, res) => {
+
 }
 
 module.exports = {
-test
+    showInfo,
+    addDevice,
+    deleteDevice,
+    valUpdate,
+    updateDevice,
+    searchDevice
 }
--- routes/device.js
+++ routes/device.js
@@ -1,7 +1,13 @@
 const router = require('express').Router()
 const deviceController = require('../controllers/device') //kiểu cữ
-const {test} =require('../controllers')  // kiểu mới thích xài cái nào xài
+const {showInfo} =require('../controllers')  // kiểu mới thích xài cái nào xài
 
-router.get('/' ,test)
-
+// Xử lý giao diện
+router.get('/' ,showInfo)
+router.get('/find-val-update/:id', deviceController.valUpdate);
+// Xử lý chức năng
+router.post('/add-device' ,deviceController.addDevice)
+router.delete('/delete-device/:id/:image' ,deviceController.deleteDevice)
+router.put('/update-device/:id' ,deviceController.updateDevice)
+router.get('/search/:name',deviceController.searchDevice);
 module.exports  = router
